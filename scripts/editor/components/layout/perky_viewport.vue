557922d4d28ed8c00469b75c05419ad3403fc999
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2023-12-11 10:17:25 +0100

    mouse control

--- scripts/editor/components/layout/perky_viewport.vue
+++ scripts/editor/components/layout/perky_viewport.vue
@@ -16,6 +16,48 @@
 
         engine.stage.addChild(graphics)
 
+
+        let isDraggable = true
+        let isDragging  = false
+        let lastX = engine.stage.x
+        let lastY = engine.stage.y
+
+
+
+        function startDrag (event) {
+            if (event.button === 1 && isDraggable) {
+                isDragging = true
+                lastX = event.offsetX
+                lastY = event.offsetY
+            }
+        }
+
+
+        function drag (event) {
+            if (isDragging) {
+                const deltaX = event.offsetX - lastX
+                const deltaY = event.offsetY - lastY
+
+                lastX = event.offsetX
+                lastY = event.offsetY
+
+                engine.stage.x += deltaX
+                engine.stage.y += deltaY
+                console.log(engine.stage.x)
+            }
+        }
+
+
+        function stopDrag () {
+            isDragging = false
+        }
+
+
+        engine.view.addEventListener('mousedown', startDrag)
+        engine.view.addEventListener('mousemove', drag)
+        engine.view.addEventListener('mouseup',   stopDrag)
+
+
         window.engine = engine
     })
 
