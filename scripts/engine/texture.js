1e35cbfbec5ac8c35760044ebd94ef5cf158a72f
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2024-01-12 11:02:44 +0100

    start texture

---
+++ scripts/engine/texture.js
@@ -0,0 +1,14 @@
+export default class Texture {
+
+    constructor () {
+        this.image  = null
+        this.width  = 0
+        this.height = 0
+    }
+
+
+    get isTexture () {
+        return true
+    }
+
+}
