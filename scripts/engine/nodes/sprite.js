fc4f8ae475cddd91680be03076fc167d21e02029
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2024-01-12 11:02:53 +0100

    start sprite

---
+++ scripts/engine/nodes/sprite.js
@@ -0,0 +1,39 @@
+import Node from '../node'
+import Vector2 from '../vector_2'
+
+
+export default class Sprite extends Node {
+
+    constructor () {
+        super()
+
+        this.setAttribute('texture', {
+            accessor: true,
+            type: 'Texture'
+        })
+
+        this.setAttribute('width', {
+            accessor: true,
+            defaultValue: 0
+        })
+
+        this.setAttribute('height', {
+            accessor: true,
+            defaultValue: 0
+        })
+
+        this.setAttribute('anchor', {
+            accessor: true,
+            defaultValue: new Vector2(1, 1)
+        })
+
+    }
+
+
+    get isSprite () {
+        return true
+    }
+
+
+}
+
