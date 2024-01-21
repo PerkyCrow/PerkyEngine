2ee92d6ac0b6f797c724801adcd5c1c5c6e4ff8d
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2024-01-21 18:57:20 +0100

    engine test

---
+++ scripts/engine/engine.test.js
@@ -0,0 +1,44 @@
+/**
+ * @jest-environment jsdom
+ */
+
+
+import Engine from './engine'
+import Node2D from './nodes/node_2d'
+
+
+describe(Engine, () => {
+
+    let engine
+
+    beforeEach(() => {
+        engine = new Engine()
+    })
+
+
+    test('constructor', () => {
+        expect(engine.world).toBeDefined()
+        expect(engine.root).toBeDefined()
+        expect(engine.view).toBeDefined()
+        expect(engine.viewport).toBeDefined()
+        expect(engine.animationLoop).toBeDefined()
+    })
+
+
+    test('init', () => {
+        const {root, view} = engine
+        const base = new Node2D()
+        const container = new Node2D()
+
+        root.addChild(base)
+        base.addChild(container)
+
+        const baseRenderer = view.renderers.get(base)
+        expect(view.scene.children).toEqual([baseRenderer.display])
+
+
+        const containerRenderer = view.renderers.get(container)
+        expect(baseRenderer.display.children).toEqual([containerRenderer.display])
+    })
+
+})
