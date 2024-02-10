ee172a0e026bec9e7a41c09c00a607001ebf6b19
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2024-02-10 11:54:57 +0100

    specs

--- scripts/engine/nodes/animation_track.test.js
+++ scripts/engine/nodes/animation_track.test.js
@@ -21,20 +21,19 @@ describe(AnimationTrack, () => {
                 duration: 1,
                 easing: 'linear',
                 target: 1
+            }, {
+                getter: () => actor.x,
+                change: value => {
+                    actor.x = value
+                },
+                duration: 1,
+                easing: 'linear',
+                target: 0
+
             }]
         })
 
         track.setReady(world)
-
-        // track.addStep({
-        //     getter: () => actor.x,
-        //     change: value => {
-        //         actor.x = value
-        //     },
-        //     duration: 1,
-        //     easing: 'linear',
-        //     target: 1
-        // })
     })
 
 
@@ -53,6 +52,15 @@ describe(AnimationTrack, () => {
         track.play()
         track.update(0.5)
         expect(actor.x).toBe(0.5)
+
+        track.update(0.5)
+        expect(actor.x).toBe(1)
+
+        track.update(0.5)
+        expect(actor.x).toBe(0.5)
+
+        track.update(0.5)
+        expect(actor.x).toBe(0)
     })
 
 
