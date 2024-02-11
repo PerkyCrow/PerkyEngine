19bba84935e98380c53d171dac33a10f32231420
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2024-02-11 12:31:26 +0100

    Physics 2D

---
+++ scripts/engine/nodes/physics_2d.js
@@ -0,0 +1,66 @@
+import Node2D from './node_2d'
+import ObservableVector2 from '../types/observable_vector_2'
+
+export default class Physics2D extends Node2D {
+
+    constructor (params = {}) {
+        super(params)
+        this.isPhysics2D = true
+
+        this.setAttribute('velocity', {
+            accessor: true,
+            serializable: true,
+            watch: true,
+            defaultValue: new ObservableVector2(0, 0),
+            value: params.velocity,
+            options: {
+                onChange: this.emitter('changed:velocity')
+            }
+        })
+
+        this.setAttribute('acceleration', {
+            accessor: true,
+            serializable: true,
+            watch: true,
+            defaultValue: new ObservableVector2(0, 0),
+            value: params.acceleration,
+            options: {
+                onChange: this.emitter('changed:acceleration')
+            }
+        })
+
+        this.setAttribute('angularVelocity', {
+            accessor: true,
+            serializable: true,
+            watch: true,
+            defaultValue: 0,
+            value: params.angularVelocity,
+            options: {
+                onChange: this.emitter('changed:angularVelocity')
+            }
+        })
+
+    }
+
+
+    update (deltaTime, elapsedTime) {
+        if (super.update(deltaTime, elapsedTime)) {
+            if (!this.acceleration.isNull()) {
+                this.velocity = this.velocity.add(this.acceleration.scale(deltaTime))
+            }
+    
+            if (!this.velocity.isNull()) {
+                this.position = this.position.add(this.velocity.scale(deltaTime))
+            }
+
+            if (this.angularVelocity) {
+                this.rotation += this.angularVelocity * deltaTime
+            }
+
+            return true
+        }
+
+        return false
+    }
+
+}
---
+++ scripts/engine/nodes/physics_2d.test.js
@@ -0,0 +1,47 @@
+import Physics2D from './physics_2d'
+import Vector2 from '../types/vector_2'
+
+
+
+describe(Physics2D, () => {
+
+    let node
+    let world = {emit: () => {}}
+
+    beforeEach(() => {
+        node = new Physics2D()
+    })
+
+
+    test('constructor', () => {
+        expect(node.isPhysics2D).toBe(true)
+        expect(node.velocity.x).toBe(0)
+        expect(node.velocity.y).toBe(0)
+        expect(node.acceleration.x).toBe(0)
+        expect(node.acceleration.y).toBe(0)
+        expect(node.angularVelocity).toBe(0)
+    })
+
+
+    test('update', () => {
+        node.setReady(world)
+
+        node.velocity = new Vector2(1, 0)
+        node.update(1)
+        expect(node.position.x).toBe(1)
+
+        node.position = new Vector2(0, 0)
+        node.velocity = new Vector2(0, 0)
+        node.acceleration = new Vector2(1, 0)
+        node.update(1)
+        expect(node.position.x).toBe(1)
+
+        node.position = new Vector2(0, 0)
+        node.velocity = new Vector2(0, 0)
+        node.acceleration = new Vector2(0, 0)
+        node.angularVelocity = 1
+        node.update(1)
+        expect(node.rotation).toBe(1)
+    })
+
+})
