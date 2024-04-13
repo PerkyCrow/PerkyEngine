b638949c9f636ea391543aeb022c659281431d76
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2024-04-13 11:10:26 +0200

    layers specs

--- scripts/engine/nodes/layer.test.js
+++ scripts/engine/nodes/layer.test.js
@@ -10,14 +10,38 @@ describe(Layer, () => {
     })
 
 
-    test('scaleToFit', () => {
+    test('scaleToContain', () => {
         const target = {width: 100, height: 100}
         node.width = 50
         node.height = 50
 
-        node.scaleToFit(target)
+        node.scaleToContain(target)
 
         expect(node.scale.x).toBe(2)
+        expect(node.scale.y).toBe(2)
+    })
+
+
+    test('scaleToCover', () => {
+        const target = {width: 100, height: 100}
+        node.width = 50
+        node.height = 50
+
+        node.scaleToCover(target)
+
+        expect(node.scale.x).toBe(2)
+        expect(node.scale.y).toBe(2)
+    })
+
+
+    test('stretchToCover', () => {
+        const target = {width: 200, height: 100}
+        node.width = 50
+        node.height = 50
+
+        node.stretchToCover(target)
+
+        expect(node.scale.x).toBe(4)
         expect(node.scale.y).toBe(2)
     })
 
